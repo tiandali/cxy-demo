90baceda0fd4c2271d54c334a90a20a9ebb55e4a
tiandali <tian.chengli@outlook.com>
2020-04-19 20:29:11 +0800

    并入代码

---
+++ src/views/identifyEntity/IdentifyEntity.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="artMain">
+    <i class="iconfont iconziyuan1"></i>
+
+    <el-input
+      type="textarea"
+      autosize
+      :placeholder="msg"
+      prefix-icon="el-icon-search"
+      @change="queryEntity"
+      v-model="inputdata"
+      @input="getInputValue"
+    ></el-input>
+    <el-button class="btnStyle" type="primary">提交</el-button>
+  </div>
+</template>
+<script>
+export default {
+  name: "IdentifyEntity",
+  data() {
+    return {
+      msg:
+        "请输出相关数据，进行实体识别\n如：玉米与传统的水稻、小麦等粮食作物相比，玉米具有很强的耐旱性、耐寒性、耐贫瘠性以及极好的环境适应性。玉米的营养价值较高，是优良的粮食作物。作为中国的高产粮食作物，玉米是畜牧业、养殖业、水产养殖业等的重要饲料来源，也是食品、医疗卫生、轻工业、化工业等的不可或缺的原料之一。 [3] 由于玉米资源极为丰富、廉价且易于获得，它们还具有许多生物活性，如抗氧化、抗肿瘤、降血糖...",
+      inputdata:
+        "请输出相关数据，进行实体识别\n如：玉米与传统的水稻、小麦等粮食作物相比，玉米具有很强的耐旱性、耐寒性、耐贫瘠性以及极好的环境适应性。玉米的营养价值较高，是优良的粮食作物。作为中国的高产粮食作物，玉米是畜牧业、养殖业、水产养殖业等的重要饲料来源，也是食品、医疗卫生、轻工业、化工业等的不可或缺的原料之一。 [3] 由于玉米资源极为丰富、廉价且易于获得，它们还具有许多生物活性，如抗氧化、抗肿瘤、降血糖..."
+    };
+  },
+  created() {},
+  methods: {
+    getInputValue(value) {
+      console.log("输入框当前的value: ", value);
+      this.inputdata = value;
+    },
+    queryEntity(value) {
+      console.log("搜索的value: ", value);
+      this.$router.push("/info");
+    }
+  }
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+.artMain {
+  min-height: calc(100vh - 80px);
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  background: #010619;
+  padding: 160px 220px;
+  i {
+    font-size: 17px;
+    color: #18191d;
+    position: absolute;
+    left: 250px;
+    top: 168px;
+    z-index: 99;
+  }
+  textarea {
+    resize: none;
+  }
+  .el-textarea__inner {
+    padding-left: 50px;
+    letter-spacing: 2px;
+  }
+  .btnStyle {
+    background: #c52275;
+    margin: 30px auto;
+    width: 120px;
+    border-color: #c52275;
+  }
+}
+</style>
---
+++ src/views/queryEntity/SearchEntity.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="searchMain">
+    <div class="inputContent">
+      <el-input
+        placeholder="请输入内容"
+        prefix-icon="el-icon-search"
+        @change="queryEntity"
+        v-model="inputdata"
+        @input="getInputValue"
+      ></el-input>
+      <div class="caseBar">
+        <span class="fontStyle">例如:</span>
+        <span class="caseStyle" v-for="item in cases" v-bind:key="item">{{item}}</span>
+        <i class="el-icon-warning-outline"></i>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "SearchEntity",
+  data() {
+    return {
+      inputdata: "",
+      cases: ["土豆", "玉米", "猪肉", "水稻"]
+    };
+  },
+  created() {},
+  methods: {
+    getInputValue(value) {
+      console.log("输入框当前的value: ", value);
+      this.inputdata = value;
+    },
+    queryEntity(value) {
+      console.log("搜索的value: ", value);
+      this.$router.push("/info");
+    }
+  },
+  computed: {}
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+.searchMain {
+  min-height: calc(100vh - 80px);
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  background: #00071a;
+  .inputContent {
+    padding-top: 200px;
+    margin: 0 392px;
+    // width: 615px;
+  }
+  .caseBar {
+    margin-top: 20px;
+    height: 30px;
+    display: flex;
+    align-content: center;
+
+    .fontStyle,
+    i {
+      color: #999;
+    }
+    .caseStyle {
+      margin-right: 20px;
+      color: #daf2f6;
+    }
+    i {
+      font-size: 24px;
+    }
+  }
+
+  .el-input {
+  }
+}
+</style>
